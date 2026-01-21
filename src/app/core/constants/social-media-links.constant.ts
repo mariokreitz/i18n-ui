8fa2b629d0a0c9cbd893f842ac5575592aef3d7d
mariokreitz <mario.kreitz@web.de>
2026-01-21 19:29:21 +0100

    feat(social-media): add social media links interface and constants

---
+++ src/app/core/constants/social-media-links.constant.ts
@@ -0,0 +1,16 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faNpm } from '@fortawesome/free-brands-svg-icons/faNpm';
+import { SocialMediaLink } from '../interfaces/social-media-link.interface';
+
+export const SOCIAL_MEDIA_LINKS: SocialMediaLink[] = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/mariokreitz/i18n-excel-manager',
+    faIcon: faGithub,
+  },
+  {
+    name: 'NPM',
+    url: 'https://www.npmjs.com/package/i18n-excel-manager',
+    faIcon: faNpm,
+  },
+];
---
+++ src/app/core/interfaces/social-media-link.interface.ts
@@ -0,0 +1,7 @@
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+
+export interface SocialMediaLink {
+  name: string;
+  url: string;
+  faIcon: IconDefinition;
+}
