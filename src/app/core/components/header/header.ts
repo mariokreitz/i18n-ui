870fb68b4f3a2005c8f2f985c18834cfbd3c71b3
mariokreitz <mario.kreitz@web.de>
2026-01-21 20:40:36 +0100

    feat(header): add social media links to header component

--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -1,9 +1,16 @@
+import { LowerCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { SOCIAL_MEDIA_LINKS } from '../../constants/social-media-links.constant';
+import { SocialMediaLink } from '../../interfaces';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [LowerCasePipe, FaIconComponent],
   templateUrl: './header.html',
   styleUrl: './header.css',
+  host: { class: 'flex justify-between items-center' },
 })
-export class Header {}
+export class Header {
+  public readonly SOCIAL_MEDIA_LINKS: SocialMediaLink[] = SOCIAL_MEDIA_LINKS;
+}
