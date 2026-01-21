ea4e764224d23b49f8c1129fd44c23b4276c8877
mariokreitz <mario.kreitz@web.de>
2026-01-21 19:09:39 +0100

    feat(home): add home component with template and styles

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RootLayout } from '../../core/layouts/root-layout/root-layout';
+
+@Component({
+  selector: 'app-home',
+  imports: [RootLayout],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export default class Home {}
