9dd1d8fe79c6a4406298cc3d915fc44fb7679c99
mariokreitz <mario.kreitz@web.de>
2026-01-21 19:17:16 +0100

    feat(home): replace placeholder with hero section

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RootLayout } from '../../core/layouts/root-layout/root-layout';
+import { Hero } from '../../features/hero/hero';
 
 @Component({
   selector: 'app-home',
-  imports: [RootLayout],
+  imports: [RootLayout, Hero],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
