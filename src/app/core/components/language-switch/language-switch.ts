54fe02eebbb0148f9cf16804e449cfb147675d02
mariokreitz <mario.kreitz@web.de>
2026-01-22 08:31:42 +0100

    feat(language-switch): add language switch component with tests

---
+++ src/app/core/components/language-switch/language-switch.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-switch',
+  imports: [],
+  templateUrl: './language-switch.html',
+  styleUrl: './language-switch.css',
+})
+export class LanguageSwitch {}
