1b28e91a0ad4634898ca54ca6ae3d4054163bfb5
mariokreitz <mario.kreitz@web.de>
2026-01-23 06:20:52 +0100

    feat(social-media): add GitHub profile URL constant

--- src/app/core/constants/social-media-links.constant.ts
+++ src/app/core/constants/social-media-links.constant.ts
@@ -2,6 +2,8 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faNpm } from '@fortawesome/free-brands-svg-icons/faNpm';
 import { SocialMediaLink } from '../interfaces';
 
+export const GITHUB_PROFILE_URL = 'https://github.com/mariokreitz';
+
 export const SOCIAL_MEDIA_LINKS: SocialMediaLink[] = [
   {
     name: 'GitHub',
