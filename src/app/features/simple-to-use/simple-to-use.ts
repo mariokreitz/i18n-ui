994ed919ef397478e8e0a8c271cc5207e4ffcb95
mariokreitz <mario.kreitz@web.de>
2026-01-23 13:37:58 +0100

    feat(simple-to-use): enhance code card examples and layout

--- src/app/features/simple-to-use/simple-to-use.ts
+++ src/app/features/simple-to-use/simple-to-use.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CodeCardBody } from '../../shared/components/code-card-body/code-card-body';
+import { CodeCardTitle } from '../../shared/components/code-card-title/code-card-title';
 import { CodeCard } from '../../shared/components/code-card/code-card';
+import { HighlightCodeDirective } from '../../shared/directives';
 
 @Component({
   selector: 'app-simple-to-use',
-  imports: [CodeCard],
+  imports: [CodeCard, CodeCardTitle, CodeCardBody, HighlightCodeDirective],
   templateUrl: './simple-to-use.html',
   styleUrl: './simple-to-use.css',
 })
