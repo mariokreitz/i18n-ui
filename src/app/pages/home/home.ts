ee47d78cb4b44e9402869ea9bf4071fb1e431cdc
mariokreitz <mario.kreitz@web.de>
2026-01-23 04:12:05 +0100

    feat(home): add features component to home layout

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RootLayout } from '../../core/layouts/root-layout/root-layout';
+import { Features } from '../../features/features/features';
 import { Hero } from '../../features/hero/hero';
 
 @Component({
   selector: 'app-home',
-  imports: [RootLayout, Hero],
+  imports: [RootLayout, Hero, Features],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
