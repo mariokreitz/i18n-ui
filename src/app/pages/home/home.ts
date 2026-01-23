add6f6cf349c0ed341c0d483c8ea2089a0e69535
mariokreitz <mario.kreitz@web.de>
2026-01-23 12:24:31 +0100

    feat(home): add SimpleToUse component to home layout

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RootLayout } from '../../core/layouts/root-layout/root-layout';
 import { Features } from '../../features/features/features';
 import { Hero } from '../../features/hero/hero';
+import { SimpleToUse } from '../../features/simple-to-use/simple-to-use';
 
 @Component({
   selector: 'app-home',
-  imports: [RootLayout, Hero, Features],
+  imports: [RootLayout, Hero, Features, SimpleToUse],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
