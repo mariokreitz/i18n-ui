a1a805bfbc39d1c53b02737e248e764db24dbbd8
mariokreitz <mario.kreitz@web.de>
2026-01-23 05:31:55 +0100

    feat(i18n): add i18nExcelManagerFeature interface

---
+++ src/app/core/interfaces/i18n-excel-manager-feature.interface.ts
@@ -0,0 +1,7 @@
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+
+export interface i18nExcelManagerFeature {
+  title: string;
+  description: string;
+  icon: IconDefinition;
+}
