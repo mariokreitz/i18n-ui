591025abb3574c27c44ea54b65f4078101630ae4
mariokreitz <mario.kreitz@web.de>
2026-01-23 17:00:41 +0100

    feat(language-switch): enhance language selection logic

--- src/app/core/components/language-switch/language-switch.ts
+++ src/app/core/components/language-switch/language-switch.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT, LowerCasePipe } from '@angular/common';
-import { Component, inject, Signal, signal, WritableSignal } from '@angular/core';
+import { Component, computed, inject, Signal, signal, WritableSignal } from '@angular/core';
 import { FaIconComponent, IconDefinition } from '@fortawesome/angular-fontawesome';
 import { faLanguage } from '@fortawesome/free-solid-svg-icons/faLanguage';
 import { Language, TranslatePipe, TranslateService } from '@ngx-translate/core';
@@ -12,17 +12,22 @@ import { Language, TranslatePipe, TranslateService } from '@ngx-translate/core';
 })
 export class LanguageSwitch {
   public readonly langIcon: IconDefinition = faLanguage;
+  public isCurrentLanguage = computed<(lang: Language) => boolean>(() => {
+    const current: string | null = this.currentLanguage();
+    return (lang: Language) => lang === current;
+  });
   private readonly _languageList: WritableSignal<readonly Language[]> = signal<readonly Language[]>([]);
   public readonly languages: Signal<readonly Language[]> = this._languageList.asReadonly();
-
   private readonly _isMenuOpen: WritableSignal<boolean> = signal<boolean>(false);
   public readonly isMenuOpen: Signal<boolean> = this._isMenuOpen.asReadonly();
-
+  private readonly _currentLanguage: WritableSignal<Language | null> = signal<Language | null>(null);
+  public readonly currentLanguage: Signal<Language | null> = this._currentLanguage.asReadonly();
   private readonly translate: TranslateService = inject(TranslateService);
   private readonly document: Document = inject(DOCUMENT);
 
   constructor() {
     this._languageList.set([...this.translate.getLangs()]);
+    this._currentLanguage.set(this.translate.getCurrentLang());
   }
 
   public toggleMenuSelection(): void {
@@ -32,6 +37,7 @@ export class LanguageSwitch {
   public onSelectLang(lang: Language): void {
     this.translate.use(lang);
     this.document.documentElement.lang = lang;
+    this._currentLanguage.set(lang);
     this._isMenuOpen.set(false);
   }
 }
