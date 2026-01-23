cd7b06e83bd432707e19c5150ea844a5076703bb
mariokreitz <mario.kreitz@web.de>
2026-01-23 05:31:48 +0100

    feat(features): implement features section with dynamic content

--- src/app/features/features/features.ts
+++ src/app/features/features/features.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import {
+  I18N_EXCEL_MANAGER_AI_POWERED_FEATURES,
+  I18N_EXCEL_MANAGER_CODE_ANALYSIS_FEATURES,
+  I18N_EXCEL_MANAGER_CORE_FEATURES,
+  I18N_EXCEL_MANAGER_DEV_EXPERIENCE_FEATURES,
+} from '../../core/constants';
+import { i18nExcelManagerFeature } from '../../core/interfaces';
+import { FeatureCard } from '../../shared/feature-card/feature-card';
 
 @Component({
   selector: 'app-features',
-  imports: [],
+  imports: [FeatureCard, TranslatePipe],
   templateUrl: './features.html',
   styleUrl: './features.css',
 })
-export class Features {}
+export class Features {
+  public readonly coreFeatures: i18nExcelManagerFeature[] = I18N_EXCEL_MANAGER_CORE_FEATURES;
+  public readonly codeAnalysisFeature: i18nExcelManagerFeature[] = I18N_EXCEL_MANAGER_CODE_ANALYSIS_FEATURES;
+  public readonly aiPoweredFeature: i18nExcelManagerFeature[] = I18N_EXCEL_MANAGER_AI_POWERED_FEATURES;
+  public readonly developerExperience: i18nExcelManagerFeature[] = I18N_EXCEL_MANAGER_DEV_EXPERIENCE_FEATURES;
+}
