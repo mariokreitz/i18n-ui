fd9224af3cc3581e7e0bca48e0b42bebca839707
mariokreitz <mario.kreitz@web.de>
2026-01-23 05:31:43 +0100

    feat(feature-card): enhance feature card layout and styles

--- src/app/shared/feature-card/feature-card.ts
+++ src/app/shared/feature-card/feature-card.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, InputSignal } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { TranslatePipe } from '@ngx-translate/core';
+import { i18nExcelManagerFeature } from '../../core/interfaces';
 
 @Component({
   selector: 'app-feature-card',
-  imports: [],
+  imports: [FaIconComponent, TranslatePipe],
   templateUrl: './feature-card.html',
   styleUrl: './feature-card.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FeatureCard {}
+export class FeatureCard {
+  public readonly feature: InputSignal<i18nExcelManagerFeature> = input.required<i18nExcelManagerFeature>();
+}
