c68739f8c6eeb49c414be83b5929703d17540c6b
mariokreitz <mario.kreitz@web.de>
2026-01-23 06:21:05 +0100

    feat(footer): enhance footer with GitHub links and icons

--- src/app/core/components/footer/footer.ts
+++ src/app/core/components/footer/footer.ts
@@ -1,10 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FaIconComponent, IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faExternalLink } from '@fortawesome/free-solid-svg-icons/faExternalLink';
+import { TranslatePipe } from '@ngx-translate/core';
+import { GITHUB_LICENSE_URL, GITHUB_PROFILE_URL, GITHUB_QUICK_LINKS } from '../../constants';
+import { SocialMediaLink } from '../../interfaces';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FaIconComponent, TranslatePipe],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Footer {}
+export class Footer {
+  public readonly githubQuickLinks: SocialMediaLink[] = GITHUB_QUICK_LINKS;
+
+  public readonly githubProfileUrl: string = GITHUB_PROFILE_URL;
+  public readonly githubLicenseUrl: string = GITHUB_LICENSE_URL;
+
+  public readonly faExt: IconDefinition = faExternalLink;
+}
