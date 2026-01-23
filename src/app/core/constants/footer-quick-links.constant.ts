fb40f8913c621632dc7a1159ac1b1a21a80f41dd
mariokreitz <mario.kreitz@web.de>
2026-01-23 06:21:01 +0100

    feat(constants): add GitHub quick links constant

---
+++ src/app/core/constants/footer-quick-links.constant.ts
@@ -0,0 +1,15 @@
+import { faExternalLink } from '@fortawesome/free-solid-svg-icons/faExternalLink';
+import { SocialMediaLink } from '../interfaces';
+
+export const GITHUB_QUICK_LINKS: SocialMediaLink[] = [
+  {
+    name: 'Changelog',
+    url: 'https://github.com/mariokreitz/i18n-excel-manager/blob/main/CHANGELOG.md',
+    faIcon: faExternalLink,
+  },
+  {
+    name: 'Github Issues',
+    url: 'https://github.com/mariokreitz/i18n-excel-manager/issues',
+    faIcon: faExternalLink,
+  },
+];
