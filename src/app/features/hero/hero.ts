5753865f4c6666d1417a10489ac9f1f09a376140
mariokreitz <mario.kreitz@web.de>
2026-01-24 03:43:45 +0100

    feat(hero): add scroll down functionality to hero section

--- src/app/features/hero/hero.ts
+++ src/app/features/hero/hero.ts
@@ -1,12 +1,8 @@
-import { NgOptimizedImage, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Signal, signal, WritableSignal } from '@angular/core';
+import { NgOptimizedImage, UpperCasePipe, ViewportScroller } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FaIconComponent, IconDefinition } from '@fortawesome/angular-fontawesome';
-import { faCircleCheck } from '@fortawesome/free-regular-svg-icons/faCircleCheck';
-import { faCopy } from '@fortawesome/free-regular-svg-icons/faCopy';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons/faChevronDown';
 import { TranslatePipe } from '@ngx-translate/core';
-import { I18N_EXCEL_MANAGER_NPM_INSTALL_COMMAND } from '../../core/constants';
-import { copyTextToClipboard } from '../../shared/utils';
 
 @Component({
   selector: 'app-hero-section',
@@ -19,21 +15,10 @@ import { copyTextToClipboard } from '../../shared/utils';
   },
 })
 export class Hero {
-  public readonly copyIcon: IconDefinition = faCopy;
-  public readonly successIcon: IconDefinition = faCircleCheck;
   public readonly chevronDownIcon: IconDefinition = faChevronDown;
-  public readonly installCommand: string = I18N_EXCEL_MANAGER_NPM_INSTALL_COMMAND;
+  private readonly viewportScroller: ViewportScroller = inject(ViewportScroller);
 
-  private readonly _isCopySuccess: WritableSignal<boolean> = signal<boolean>(false);
-  public readonly isCopySuccess: Signal<boolean> = this._isCopySuccess.asReadonly();
-
-  public async copyToClipboard(): Promise<void> {
-    try {
-      await copyTextToClipboard(this.installCommand);
-      this._isCopySuccess.set(true);
-      setTimeout(() => this._isCopySuccess.set(false), 2000);
-    } catch (e) {
-      console.error('Failed to copy text to clipboard:', e);
-    }
+  public scrollToGettingStarted(): void {
+    this.viewportScroller.scrollToPosition([0, window.innerHeight]);
   }
 }
