17206fd0b3c720501d6b7fc91135e96706274426
mariokreitz <mario.kreitz@web.de>
2026-01-24 04:55:47 +0100

    feat(header): add reload button with accessibility support

--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -1,12 +1,17 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { LanguageSwitch } from '../language-switch/language-switch';
 import { NavSocial } from '../navigation/nav-social/nav-social';
 
 @Component({
   selector: 'app-header',
-  imports: [NgOptimizedImage, NavSocial, LanguageSwitch],
+  imports: [NgOptimizedImage, NavSocial, LanguageSwitch, TranslatePipe],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  public reloadPage(): void {
+    window.location.reload();
+  }
+}
