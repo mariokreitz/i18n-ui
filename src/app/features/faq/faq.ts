f3d21ecb735e8e4e208b54bc6bff8a0ac8a648b1
mariokreitz <mario.kreitz@web.de>
2026-01-24 03:28:07 +0100

    feat(faq): enhance FAQ section with dynamic Q&A items

--- src/app/features/faq/faq.ts
+++ src/app/features/faq/faq.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { FAQ_ITEMS } from '../../core/constants/faq.constant';
+import { FaqItem } from '../../core/interfaces';
 
 @Component({
   selector: 'app-faq',
   imports: [TranslatePipe],
   templateUrl: './faq.html',
   styleUrl: './faq.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Faq {}
+export class Faq {
+  public readonly faqItems: readonly FaqItem[] = FAQ_ITEMS;
+}
