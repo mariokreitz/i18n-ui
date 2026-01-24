cfe4d55759a8beb6f2ddc5a6dd88f3c4f715eefa
mariokreitz <mario.kreitz@web.de>
2026-01-24 04:11:47 +0100

    feat(faq): add fade-in directive and update styles

--- src/app/features/faq/faq.ts
+++ src/app/features/faq/faq.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { FAQ_ITEMS } from '../../core/constants/faq.constant';
 import { FaqItem } from '../../core/interfaces';
+import { FadeInDirective } from '../../shared/directives';
 
 @Component({
   selector: 'app-faq',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, FadeInDirective],
   templateUrl: './faq.html',
   styleUrl: './faq.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
