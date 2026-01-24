3854002a3cd3792f38c1cd609b1a3d7eec75c400
mariokreitz <mario.kreitz@web.de>
2026-01-24 04:55:53 +0100

    feat(layout): add back to top button component

--- src/app/core/layouts/root-layout/root-layout.ts
+++ src/app/core/layouts/root-layout/root-layout.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { BackToTopComponent } from '../../../shared/components/back-to-top/back-to-top';
 import { ToastComponent } from '../../../shared/components/toast/toast';
 import { Footer } from '../../components/footer/footer';
 import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-root-layout',
-  imports: [Header, Footer, ToastComponent, TranslatePipe],
+  imports: [Header, Footer, ToastComponent, BackToTopComponent, TranslatePipe],
   templateUrl: './root-layout.html',
   styleUrl: './root-layout.css',
   host: { class: 'min-h-screen flex flex-col' },
