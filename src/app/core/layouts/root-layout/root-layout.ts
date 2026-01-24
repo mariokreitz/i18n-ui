15dc34b4fc841d50eeeb2f3c20612a7fd01b3584
mariokreitz <mario.kreitz@web.de>
2026-01-24 04:18:17 +0100

    feat(layout): add toast component to root layout

--- src/app/core/layouts/root-layout/root-layout.ts
+++ src/app/core/layouts/root-layout/root-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ToastComponent } from '../../../shared/components/toast/toast';
 import { Footer } from '../../components/footer/footer';
 import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-root-layout',
-  imports: [Header, Footer],
+  imports: [Header, Footer, ToastComponent],
   templateUrl: './root-layout.html',
   styleUrl: './root-layout.css',
   host: { class: 'min-h-screen flex flex-col' },
