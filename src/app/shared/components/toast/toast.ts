f3dfe2c3dc0550557b8ca153a11d12e77bdd0f4a
mariokreitz <mario.kreitz@web.de>
2026-01-24 04:24:08 +0100

    feat(toast): add translation support for toast messages

--- src/app/shared/components/toast/toast.ts
+++ src/app/shared/components/toast/toast.ts
@@ -3,19 +3,20 @@ import { FaIconComponent, IconDefinition } from '@fortawesome/angular-fontawesom
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons/faCheckCircle';
 import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons/faInfoCircle';
+import { TranslatePipe } from '@ngx-translate/core';
 import { Toast, ToastService } from '../../../core/services';
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.html',
   standalone: true,
-  imports: [FaIconComponent],
+  imports: [FaIconComponent, TranslatePipe],
 })
 export class ToastComponent {
   public readonly successIcon: IconDefinition = faCheckCircle;
   public readonly infoIcon: IconDefinition = faInfoCircle;
   public readonly errorIcon: IconDefinition = faExclamationCircle;
-  public readonly visible: Signal<boolean> = computed(() => !!this.toast());
   private readonly toastService: ToastService = inject(ToastService);
   public readonly toast: Signal<Toast | null> = this.toastService.toast;
+  public readonly visible: Signal<boolean> = computed(() => !!this.toast());
 }
