15d9cfefce864a2d9043281e7a3a458be4db54d3
mariokreitz <mario.kreitz@web.de>
2026-03-04 02:43:43 +0100

    refactor(toast): implement OnPush change detection strategy

--- src/app/shared/components/toast/toast.ts
+++ src/app/shared/components/toast/toast.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, Signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, Signal } from '@angular/core';
 import { FaIconComponent, IconDefinition } from '@fortawesome/angular-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons/faCheckCircle';
 import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle';
@@ -9,14 +9,14 @@ import { Toast, ToastService } from '../../../core/services';
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.html',
-  standalone: true,
   imports: [FaIconComponent, TranslatePipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToastComponent {
   public readonly successIcon: IconDefinition = faCheckCircle;
   public readonly infoIcon: IconDefinition = faInfoCircle;
   public readonly errorIcon: IconDefinition = faExclamationCircle;
+  public readonly visible: Signal<boolean> = computed(() => !!this.toast());
   private readonly toastService: ToastService = inject(ToastService);
   public readonly toast: Signal<Toast | null> = this.toastService.toast;
-  public readonly visible: Signal<boolean> = computed(() => !!this.toast());
 }
