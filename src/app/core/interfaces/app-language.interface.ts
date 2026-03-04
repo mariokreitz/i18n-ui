6772709e1fbe51dd7326d682ef9e3cc354a6769e
mariokreitz <mario.kreitz@web.de>
2026-03-04 02:42:08 +0100

    refactor(language): update LocaleData type for registerLocaleData

--- src/app/core/interfaces/app-language.interface.ts
+++ src/app/core/interfaces/app-language.interface.ts
@@ -1,3 +1,5 @@
+import { registerLocaleData } from '@angular/common';
+
 export type LanguageCode = 'de' | 'en' | 'fr' | 'es' | 'pt' | 'tr';
 
-export type LocaleData = [unknown, unknown];
+export type LocaleData = [Parameters<typeof registerLocaleData>[0], Parameters<typeof registerLocaleData>[2]];
